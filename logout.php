b95f9815bff5867cd96b1b575c52090f871d94e2
De Luna, Jhon Mark Bautista <jjmbdeluna@gmail.com>
2025-10-16 17:46:26 +0800

    Add files via upload
    updated files

---
+++ logout.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+// Destroy all session data (log out the admin)
+session_unset();
+session_destroy();
+
+// Redirect back to login page
+header("Location: quick-links.html");
+exit();
+?>
